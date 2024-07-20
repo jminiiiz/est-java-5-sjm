af124d53d63435f5544ef4c61c6324a1a5211836
juungmiin <ajmkk7@gmail.com>
2024-07-21 06:05:27 +0900

    commit

---
+++ src/main/java/com/example/estspringjpamysql/EstSpringJpaMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.estspringjpamysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstSpringJpaMysqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EstSpringJpaMysqlApplication.class, args);
+    }
+
+}
